79517f08c91f3d50c91a845ca6abe7edd80c667d
Arzu Ünüvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-08 14:48:37 +0300

    first commit
    Homeworks

---
+++ ödev1/yashesaplama.php
@@ -0,0 +1,17 @@
+<?php 
+
+$age = 25;
+
+if ($age<=14):
+	print_r("Çocuk");
+elseif ($age<=24):
+	print_r("Genç");
+elseif ($age<=64):
+	print_r("Yetişkin");
+elseif ($age>=65):
+	print_r("Yaşlı");
+else:
+	print_r("İhtiyar");
+endif;
+
+?>
---
+++ ödev2/kalorihesaplama.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$sex = "female";
+$weight = 60;
+$height = 170;
+$age = 30;
+
+
+switch ($sex){
+case 'female':
+$sex= (10 * 60 ) + (6.25 * 170) - (5 * 30) - 161;
+echo "<p>Your estimated daily metabolic rate is $sex </p>";
+ 
+break;
+case 'male':
+$sex= (10 *55) + (6.25 * 170) - (5 * 25) + 5;
+echo "<p>Your estimated daily metabolic rate is $sex </p>";
+
+}
+
+?>
+

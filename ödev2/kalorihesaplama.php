25c601c1f2dc9ba07d1051ad9259d400f635de6e
arzunuvar <62670222+arzunuvar@users.noreply.github.com>
2021-02-08 18:15:40 +0300

    Update kalorihesaplama.php

--- ödev2/kalorihesaplama.php
+++ ödev2/kalorihesaplama.php
@@ -9,15 +9,14 @@ $age = 30;
 
 switch ($sex){
 case 'female':
-$sex= (10 * 60 ) + (6.25 * 170) - (5 * 30) - 161;
-echo "<p>Your estimated daily metabolic rate is $sex </p>";
+$calorie= (10 * $weight) + (6.25 * $height) - (5 * $age) - 161;
+echo "<p>Your estimated daily metabolic rate is $calorie </p>";
  
 break;
 case 'male':
-$sex= (10 *55) + (6.25 * 170) - (5 * 25) + 5;
-echo "<p>Your estimated daily metabolic rate is $sex </p>";
+$calorie= (10 * $weight) + (6.25 * $height) - (5 * $age) + 5;
+echo "<p>Your estimated daily metabolic rate is $calorie </p>";
 
 }
 
 ?>
-
